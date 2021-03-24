3bfdaf95a2b91255dcd89655662cd5e1b9e87478
FamousJamous <jsd461@gmail.com>
2021-03-24 10:20:24 -0400

    Add files via upload

--- minimax/minimax.go
+++ minimax/minimax.go
@@ -14,22 +14,44 @@ type MiniMaxMove interface { }
 type MiniMaxGame interface {
   GetAllMoves() []MiniMaxMove
   GetScore() Score
-  MakeMove(move MiniMaxMove) error
+  MakeMove(move MiniMaxMove)
   UndoMove()
   String() string
+  StringKey() string
 }
 
-func MiniMax(game MiniMaxGame,
-             minimize bool,
-             depth int,
-             maxDepth int) (MiniMaxMove, Score) {
-  moves := game.GetAllMoves()
+type MiniMaxState struct {
+  game MiniMaxGame
+  minimizeStart bool
+  maxDepth int
+  visited map[string]Score
+}
+
+func MakeState(game MiniMaxGame, minimize bool, maxDepth int) *MiniMaxState {
+  return &MiniMaxState{game, minimize, maxDepth, make(map[string]Score)}
+}
+
+func MiniMax(game MiniMaxGame, minimize bool, maxDepth int) MiniMaxMove {
+  state := MakeState(game, minimize, maxDepth)
+  move, _ := state.run(minimize, 1)
+  return move
+}
+
+func (state *MiniMaxState) GetMove() MiniMaxMove {
+  state.visited = make(map[string]Score)
+  move, _ := state.run(state.minimizeStart, 1)
+  return move
+}
+
+func (state *MiniMaxState) run(minimize bool, depth int) (MiniMaxMove, Score) {
+  moves := state.game.GetAllMoves()
   if len(moves) == 0 {
-    return nil, game.GetScore()
+    return nil, state.game.GetScore()
   }
-  bestMove, bestScore := tryMove(game, minimize, depth, maxDepth, moves[0])
+  bestMove, bestScore :=
+      state.tryMove(minimize, depth, moves[0])
   for i, n := 1, len(moves); i < n; i++ {
-    move, score := tryMove(game, minimize, depth, maxDepth, moves[i])
+    move, score := state.tryMove(minimize, depth, moves[i])
     if (minimize && score < bestScore) || (!minimize && score > bestScore) {
       bestMove = move
       bestScore = score
@@ -38,18 +60,23 @@ func MiniMax(game MiniMaxGame,
   return bestMove, bestScore
 }
 
-func tryMove(game MiniMaxGame,
-             minimize bool,
-             depth int,
-             maxDepth int,
-             move MiniMaxMove) (MiniMaxMove, Score) {
-  if ok := game.MakeMove(move); ok != nil {
-    panic(ok.Error())
+func (state *MiniMaxState) tryMove(
+    minimize bool, depth int, move MiniMaxMove) (MiniMaxMove, Score) {
+  state.game.MakeMove(move)
+  key := state.game.StringKey()
+  if score, ok := state.visited[key]; ok {
+    // Already visited
+    state.game.UndoMove()
+    return move, score
   }
-  score := game.GetScore()
-  if score != MaxScore && score != MinScore && depth != maxDepth {
-    _, score = MiniMax(game, !minimize, depth + 1, maxDepth)
+  score := state.game.GetScore()
+  // Mark visited before recursive calls
+  state.visited[key] = score
+  if score != MaxScore && score != MinScore && depth != state.maxDepth {
+    _, score = state.run(!minimize, depth + 1)
+    // Update score if we made recursive calls
+    state.visited[key] = score
   }
-  game.UndoMove()
+  state.game.UndoMove()
   return move, score
 }
